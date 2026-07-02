5252b2d847f0c5138cfb960ce0e54acdace94cb0
x-LANsolo-x <kumarutkarsh688@gmail.com>
2026-07-02 09:48:53 +0530

    Configure CORS, ALLOWED_HOSTS, and add gunicorn requirements for Render deployment

--- backend/CUintranet/cuintranet/settings.py
+++ backend/CUintranet/cuintranet/settings.py
@@ -29,15 +29,25 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 if os.environ.get('RENDER_EXTERNAL_HOSTNAME'):
     ALLOWED_HOSTS.append(os.environ.get('RENDER_EXTERNAL_HOSTNAME'))
-#CORS_ALLOW_ALL_ORIGINS = True
-#CORS_ORIGIN_ALLOW_ALL = True
-CORS_ALLOW_ALL_ORIGINS = True
+FRONTEND_URL = os.environ.get('FRONTEND_URL')
+
+if FRONTEND_URL:
+    CORS_ALLOWED_ORIGINS = [
+        "http://localhost:5173",
+        "http://localhost:3000",
+        FRONTEND_URL,
+    ]
+else:
+    CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOW_CREDENTIALS = True
 
 CSRF_TRUSTED_ORIGINS = [
+    "http://localhost:5173",
     "http://localhost:3000",
-    # other origins
 ]
+if FRONTEND_URL:
+    CSRF_TRUSTED_ORIGINS.append(FRONTEND_URL)
 # Application definition
 
 INSTALLED_APPS = [
